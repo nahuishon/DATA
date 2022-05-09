55abb69ba6c3cb837c153e402a9e78f565d9a68f
nahuishon <shonn620@newschool.edu>
2022-05-09 13:25:40 -0400

    ㄴ

---
+++  script.js
@@ -0,0 +1,75 @@
+
+var msg; 
+msg = "Hello JavaScript!"; // assigned a string value
+alert(msg); // access a variable
+
+const cars = ["2020", "2021"];
+
+let text = "";
+for (let x of cars) {
+text += x + "<br>";
+}
+
+document.getElementById("demo").innerHTML = text;
+
+
+const xlabels = [];
+const ydeath = [];
+
+chartIt();
+
+
+async function chartIt() {
+const data =  await getData();
+const ctx = document.getElementById('chart').getContext('2d');
+
+const myChart = new Chart(ctx, {
+type: 'bar',
+data: {
+labels:  data.xs,
+datasets: [{
+    label: '# of Votes',
+    data: data.ys,
+    backgroundColor:
+        'black',
+      
+    borderColor: 
+        'rgba(255, 99, 132, 1)',
+    borderWidth: 1
+}]
+},
+});
+}
+
+
+async function getData() {
+
+const xs = [];
+const ys = [];
+
+const response = await fetch('national-history.csv');
+const data = await response.text();
+
+const table = data.split('\n').slice(1);
+table.forEach(row => {
+const columns = row.split(',');
+const date = columns[0];
+xs.push(date);
+const death = columns[1];
+ys.push(parseFloat(death) + 1000);
+console.log(date, death);
+});
+return {xs,ys};
+}
+
+
+function myFunction() {
+let text;
+let person = prompt("Please enter your name:", "Harry Potter");
+if (person == null || person == "") {
+text = "User cancelled the prompt.";
+} else {
+text = "Hello " + person + "! How are you today?";
+}
+document.getElementById("demo").innerHTML = text;
+}
